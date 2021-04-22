ca4a7551ffcb0d35d568bb5c76280a5ccc0c9234
more0 <more0@DESKTOP-F33AG9K>
2021-04-22 15:13:34 +0530

    First commit Uploading all files

---
+++ Product-1/src/main/java/com/spring/entity/Product.java
@@ -0,0 +1,24 @@
+package com.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name ="PRODUCT_TBL")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private int quantity;
+	private double price;
+
+}
